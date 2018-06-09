73518ca9dd27534017bf8af4bc9c9c42582e7a44
Ilona Sellenberkova <ilona.sell@gmail.com>
2018-06-10 00:26:15 +0200

    show and style not done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # need to redirect it does not do anything on that page
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    # update task already stored in db, access change data to user another than specify below
+    @task = Task.find(task_params)
+
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    # back to home page
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    # added completed for user when click on checkbox
+    params.require(:task).permit(:title, :details)
+  end
+
+end
